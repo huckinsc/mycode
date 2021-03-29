6f27cd59281729436886591281b8e336c597379c
Chris Huckins <chris.huckins@tlgcohort.com>
2021-03-29 19:17:43 +0000

    init push

---
+++ lab_input/challange1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+user_name = input("What is your name:")
+day = input("What day is it:")
+print ("Hello, " + user_name + "! Happy " + day + ".")
+print(f"Hello, {user_name}! Happy {day}.")
+
---
+++ lab_input/iptaker02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+user_input = input("Please enter an IPv4 IP address:")
+
+print("You told me the TPv$ address is:" + user_input)
+
+vendor_name = input("Please enter a vendor name:")
+print("The vendor name is" + vendor_name)
+
