0b6948fda4c5b5e7cce55c1db5712c17928ff8c8
Chris Huckins <chris.huckins@tlgcohort.com>
2021-03-31 19:41:01 +0000

    lab 38

---
+++ catfacts/facts03.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import requests
+
+def main():
+    r = requests.get('https://cat-fact.herokuapp.com/facts')
+    for catfact in r.json():
+        print(catfact.get("text"))
+
+main()
