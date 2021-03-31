2896c1c04af6f782e36ac59a951ccede3c342646
Chris Huckins <chris.huckins@tlgcohort.com>
2021-03-31 20:00:32 +0000

    lab 38

--- catfacts/facts03.py
+++ catfacts/facts03.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
 import requests
+import random
 
 def main():
     r = requests.get('https://cat-fact.herokuapp.com/facts')
     for catfact in r.json():
         print(catfact.get("text"))
+
+    print("\nlab 38 challange 1\n")
+    print(r.status_code)
+
+    print("\nlab 38 challange 2\n")
+    print(random.choice(r.json()).get("text"))
 
 main()
